7b9c6efce2f7a1266cace9e1948ea74e2caea970
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-10-20 03:40:06 +0800

    feat: add ci aws

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -5,5 +5,9 @@ import "sst"
 export {}
 declare module "sst" {
   export interface Resource {
+    "jbecpum": {
+      "type": "sst.aws.Nextjs"
+      "url": string
+    }
   }
 }
--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,24 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
       name: "jbecpum-web",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      providers: {
+        aws: {
+          profile: input?.stage === "production" ? "production" : "dev",
+        },
+      },
     };
   },
   async run() {
-    new sst.aws.Nextjs("jbecpum");
+    new sst.aws.Nextjs("jbecpum", {
+      domain: {
+        name: "jbecpum.org",
+        redirects: ["www.jbecpum.org"],
+        dns: sst.cloudflare.dns(),
+      },
+    });
   },
 });
