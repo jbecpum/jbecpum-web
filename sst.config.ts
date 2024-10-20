49b984e65d0d6acc866fcb469c8d4d084f0d979b
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-10-20 19:25:01 +0800

    feat: remove profiles

--- sst.config.ts
+++ sst.config.ts
@@ -3,11 +3,11 @@ export default $config({
   app(input) {
     return {
       name: "jbecpum-web",
-      removal: input?.stage === "production" ? "retain" : "remove",
+      // removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
       providers: {
         aws: {
-          profile: input?.stage === "production" ? "production" : "dev",
+          // profile: input?.stage === "production" ? "production" : "dev",
           region: "ap-southeast-1",
         },
       },
