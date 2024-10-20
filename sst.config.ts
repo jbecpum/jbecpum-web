36f039bdc97267eb9ca4e1a881583e45f4aade01
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-10-20 20:22:08 +0800

    feat: check pr

--- sst.config.ts
+++ sst.config.ts
@@ -3,11 +3,9 @@ export default $config({
   app(input) {
     return {
       name: "jbecpum-web",
-      // removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
       providers: {
         aws: {
-          // profile: input?.stage === "production" ? "production" : "dev",
           region: "ap-southeast-1",
         },
       },
